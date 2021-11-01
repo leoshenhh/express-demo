3e571ed4da985ea0ffbc943de8a61360a842d3bd
Leo Shen <343206898@qq.com>
2021-11-01 18:37:22 +0800

    add typescript

---
+++ app2.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('你好!');
+});
+
+app.post('/',(request,response) => {
+
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
